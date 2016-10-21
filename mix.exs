ab6a86130236037c42772909fad79c770d6bd0be
Mauricio Voto <mauriciovoto@gmail.com>
2016-10-21 00:23:53 -0200

    add jsx as deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule GotBot.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :trot, :nadia]]
+    [applications: [:logger, :trot, :nadia, :exjsx]]
   end
 
   # Dependencies can be Hex packages:
