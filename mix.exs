e68221a3ac1575c77c0374f683776dfd4c27f162
Mauricio Voto <mauriciovoto@gmail.com>
2016-10-21 00:33:18 -0200

    remove exjsx from dep

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule GotBot.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :trot, :nadia, :exjsx]]
+    [applications: [:logger, :trot, :nadia]]
   end
 
   # Dependencies can be Hex packages:
